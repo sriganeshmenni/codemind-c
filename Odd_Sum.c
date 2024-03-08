daabf8545b14135e79ad69940afc51b99067cd47
sriganeshmenni <147472350+sriganeshmenni@users.noreply.github.com>
2024-03-08 22:12:19 +0530

    #include<stdio.h>  int main(){      int n;      scanf("%d",&n);      int a[n];      for(int i=0;i<n;i++){          scanf("%d",&a[i]);      }      int s=0;      for(int i=0;i<n;i++){          if(a[i]%2 != 0)              s += a[i];
         }
         printf("%d",s);
     }

---
+++ Odd_Sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main(){
+     int n;
+     scanf("%d",&n);
+     int a[n];
+     for(int i=0;i<n;i++){
+         scanf("%d",&a[i]);
+     }
+     int s=0;
+     for(int i=0;i<n;i++){
+         if(a[i]%2 != 0)
+             s += a[i];
+         
+     }
+     printf("%d",s);
+ }
